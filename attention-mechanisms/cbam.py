2acb51b3f83bbc10b663d9ba0bcfb48b23930432
changzy00 <changzy@pku.org.cn>
2023-05-28 00:30:16 +0800

    add cbam

---
+++ attention-mechanisms/cbam.py
@@ -0,0 +1,65 @@
+""" 
+PyTorch implementation of CBAM: Convolutional Block Attention Module
+
+As described in https://arxiv.org/pdf/1807.06521
+
+The attention mechanism is achieved by using two different types of attention gates: 
+channel-wise attention and spatial attention. The channel-wise attention gate is applied 
+to each channel of the input feature map, and it allows the network to focus on the most 
+important channels based on their spatial relationships. The spatial attention gate is applied 
+to the entire input feature map, and it allows the network to focus on the most important regions 
+of the image based on their channel relationships.
+"""
+
+
+
+import torch
+from torch import nn
+
+class ChannelAttention(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return x * self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        out = torch.concat([avg_out, max_out], dim=1)
+        out = self.conv(out)
+        return x * self.sigmoid(out) 
+    
+class CBAM(nn.Module):
+    def __init__(self, channel, reduction=16, kernel_size=7):
+        super().__init__()
+        self.ca = ChannelAttention(channel, reduction)
+        self.sa = SpatialAttention(kernel_size)
+    
+    def forward(self, x):
+        x = self.ca(x)
+        x = self.sa(x)
+        return x
+    
+if __name__ == "__main__":
+    x = torch.randn(2, 64, 32, 32)
+    attn = CBAM(64)
+    y = attn(x)
+    print(y.shape)
