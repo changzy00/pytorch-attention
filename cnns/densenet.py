bc6046dfd45e2fe9fdc0625f9f0514d4e561f0dc
changzy00 <changzy@pku.org.cn>
2023-06-09 14:24:50 +0800

    add densenet

---
+++ cnns/densenet.py
@@ -0,0 +1,105 @@
+""" 
+PyTorch implementation of Densely Connected Convolutional Networks
+
+As described in http://arxiv.org/abs/1608.06993v5
+
+A DenseNet is a type of convolutional neural network that utilises dense connections between 
+layers, through Dense Blocks, where we connect all layers (with matching feature-map sizes) directly 
+with each other. To preserve the feed-forward nature, each layer obtains additional inputs from all
+ preceding layers and passes on its own feature-maps to all subsequent layers.
+"""
+
+import torch
+import torch.nn as nn
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, growth_rate):
+        super().__init__()
+        inner_channel = 4 * growth_rate
+        self.bottle_neck = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, inner_channel, kernel_size=1, bias=False),
+            nn.BatchNorm2d(inner_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inner_channel, growth_rate, kernel_size=3, padding=1, bias=False)
+        )
+
+    def forward(self, x):
+        return torch.cat([x, self.bottle_neck(x)], 1)
+
+class Transition(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.down_sample = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            nn.AvgPool2d(2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.down_sample(x)
+
+class DenseNet(nn.Module):
+    def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_class=1000):
+        super().__init__()
+        self.growth_rate = growth_rate
+        inner_channels = 2 * growth_rate
+        self.conv1 = nn.Conv2d(3, inner_channels, kernel_size=7, stride=2, padding=3) # 这是针对ImageNet数据集,如果想在CIFAR数据集上
+        self.bn1 = nn.BatchNorm2d(inner_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.features = nn.Sequential()
+
+        for index in range(len(nblocks) - 1):
+            self.features.add_module("dense_block_layer_{}".format(index), self._make_dense_layers(block, inner_channels, nblocks[index]))
+            inner_channels += growth_rate * nblocks[index]
+
+            out_channels = int(reduction * inner_channels) # int() will automatic floor the value
+            self.features.add_module("transition_layer_{}".format(index), Transition(inner_channels, out_channels))
+            inner_channels = out_channels
+
+        self.features.add_module("dense_block{}".format(len(nblocks) - 1), self._make_dense_layers(block, inner_channels, nblocks[len(nblocks)-1]))
+        inner_channels += growth_rate * nblocks[len(nblocks) - 1]
+        self.features.add_module('bn', nn.BatchNorm2d(inner_channels))
+        self.features.add_module('relu', nn.ReLU(inplace=True))
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc = nn.Linear(inner_channels, num_class)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        out = self.features(out)
+        out = self.avgpool(out)
+        out = out.view(out.size()[0], -1)
+        out = self.fc(out)
+        return out
+
+    def _make_dense_layers(self, block, in_channels, nblocks):
+        dense_block = nn.Sequential()
+        for index in range(nblocks):
+            dense_block.add_module('bottle_neck_layer_{}'.format(index), block(in_channels, self.growth_rate))
+            in_channels += self.growth_rate
+        return dense_block
+
+def densenet121():
+    return DenseNet(Bottleneck, [6,12,24,16], growth_rate=32)
+
+def densenet169():
+    return DenseNet(Bottleneck, [6,12,32,32], growth_rate=32)
+
+def densenet201():
+    return DenseNet(Bottleneck, [6,12,48,32], growth_rate=32)
+
+def densenet161():
+    return DenseNet(Bottleneck, [6,12,36,24], growth_rate=48)
+
+if __name__ == "__main__":
+    x = torch.randn(2, 3, 223, 224)
+    model = densenet121()
+    y = model(x)
+    print(y.shape)
